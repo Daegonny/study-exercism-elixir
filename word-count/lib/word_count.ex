833b2aa2810526e91f1e04010ff04ec75447d73c
Daegonny <igorchagasm@hotmail.com>
2021-09-03 08:48:15 -0300

    Usa comprehension lists

--- word-count/lib/word_count.ex
+++ word-count/lib/word_count.ex
@@ -1,20 +1,28 @@
 defmodule WordCount do
   @doc """
   Count the number of words in the sentence.
-
+  
   Words are compared case-insensitively.
   """
   @spec count(String.t()) :: map
   def count(sentence) do
-	String.split(sentence, [" ", "_"], trim: true)
-	|> Enum.map(&String.downcase/1)
-	|> Enum.map(&clean/1)
-	|> Enum.filter(fn x -> x != "" end)
+    sentence
+    |> get_cleaned_words()
+    |> Enum.filter(&(&1 != ""))
     |> Enum.frequencies()
+  end
+
+  @spec get_cleaned_words(String.t()) :: [String.t()]
+  defp get_cleaned_words(sentence) do
+    for word <- String.split(sentence, [" ", "_"], trim: true) do
+      word
+      |> String.downcase()
+      |> clean()
+    end
   end
 
   @spec clean(String.t()) :: String.t()
   defp clean(string) do
-	String.replace(string, ~r/[.,:;!?ºª§@$#%"^~'`&*()<>='"\+\|\\{\}\[\]\\\/]+/, "")
+    String.replace(string, ~r/[.,:;!?ºª§@$#%"^~'`&*()<>='"\+\|\\{\}\[\]\\\/]+/, "")
   end
 end
