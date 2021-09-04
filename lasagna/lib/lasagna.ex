ab63b216c90ac326daa7fbd30a59aa7a9be5fc92
Daegonny <igorchagasm@hotmail.com>
2021-09-04 17:55:55 -0300

    adiciona exercício da lasanha

--- lasagna/lib/lasagna.ex
+++ lasagna/lib/lasagna.ex
@@ -1,11 +1,26 @@
 defmodule Lasagna do
-  # TODO: define the 'expected_minutes_in_oven/0' function
+  # TODO: define the 'expected_minutes_in_oven/0' function]
+  def expected_minutes_in_oven() do
+    40
+  end
 
   # TODO: define the 'remaining_minutes_in_oven/1' function
+  def remaining_minutes_in_oven(elapsed_minutes) do
+    expected_minutes_in_oven() - elapsed_minutes
+  end
 
   # TODO: define the 'preparation_time_in_minutes/1' function
+  def preparation_time_in_minutes(layers) do
+    layers * 2
+  end
 
   # TODO: define the 'total_time_in_minutes/2' function
+  def total_time_in_minutes(layers, elapsed_minutes) do
+    elapsed_minutes + preparation_time_in_minutes(layers)
+  end
 
   # TODO: define the 'alarm/0' function
+  def alarm() do
+    "Ding!"
+  end
 end
